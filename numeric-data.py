1efb909e763f45cf8c97f130e3e250891f0e11b8
rafif03 <134936696+rafif03@users.noreply.github.com>
2025-08-21 20:03:32 +0700

    upload tugas 1

---
+++ numeric-data.py
@@ -0,0 +1,26 @@
+print("Python has three numeric types: int, float, and complex\n")
+
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)) + "\n")
+
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue))+ "\n")
+
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue))+ "\n")
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue))+ "\n")
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue))+ "\n")
